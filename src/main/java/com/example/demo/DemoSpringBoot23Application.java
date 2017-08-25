63489b8962ed3e37d25bf9eed15d4251721dbcb3
nakelia roseman <nakelia78@gmail.com>
2017-08-24 22:29:03 -0400

    First Commit(Custom Error Page)

---
+++ src/main/java/com/example/demo/DemoSpringBoot23Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot23Application.class, args);
+	}
+}
